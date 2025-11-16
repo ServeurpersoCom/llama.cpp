2f523555c69e4a35c5f87f1792165b70940603a4
Pascal <admin@serveurperso.com>
2025-11-16 08:09:31 +0100

    wip: personal defaults for testing-branch (website only)

--- tools/server/webui/src/lib/constants/settings-config.ts
+++ tools/server/webui/src/lib/constants/settings-config.ts
@@ -13,9 +13,9 @@ export const SETTING_CONFIG_DEFAULT: Record<string, string | number | boolean> =
 	askForTitleConfirmation: false,
 	pasteLongTextToFileLen: 2500,
 	pdfAsImage: false,
-	showModelInfo: false,
+	showModelInfo: true,
 	renderUserContentAsMarkdown: false,
-	modelSelectorEnabled: false,
+	modelSelectorEnabled: true,
 	// make sure these default values are in sync with `common.h`
 	samplers: 'top_k;typ_p;top_p;min_p;temperature',
 	temperature: 0.8,
